bdbaa92c44c8c665c01453cc3c817474d949e095
Shpitzick <>
2013-04-12 15:43:35 +0300

    Indev 0.3.1

--- src/org/dosimonline/WorldCredits.java
+++ src/org/dosimonline/WorldCredits.java
@@ -3,15 +3,19 @@ import it.randomtower.engine.World;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class WorldCredits extends World
 {
-    public WorldCredits (int id, GameContainer gc)
+    private Image heart;
+    
+    public WorldCredits (int id, GameContainer gc) throws SlickException
     {
         super (id, gc);
+        heart = new Image("org/dosimonline/res/heart.png");
     }
 
     @Override
@@ -30,5 +34,6 @@ public class WorldCredits extends World
         g.drawString("Graphics: Tomer Ginzburg", 40, 60);
         g.drawString("Beta (Or shall I say Indev?) testers: Michael Puniansky, Martin Korotkov", 40, 80);
         g.drawString("Music: Makche (Alleviation is played at the background)", 40, 100);
+        for (int x = 0; x < 1280; x += heart.getWidth()) {g.drawImage(heart, x, 862 - heart.getHeight());}
     }
 }
